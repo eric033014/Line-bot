704aa5a98d97c1916bcf816a764be12bcd80341b
eric <eric0330eric@gmail.com>
2017-09-15 16:21:27 +0800

    1816

--- app.py
+++ app.py
@@ -81,9 +81,9 @@ def eyny_movie():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == "eric":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="Hello Eric"))
+        #line_bot_api.reply_message(
+        #    event.reply_token,
+        #    TextSendMessage(text="Hello Eric"))
         content = crawer()
         line_bot_api.reply_message(
             event.reply_token,
