594d64ff843bf8bdb5f0d6d53e3b4d98f7dc75d8
eric <eric0330eric@gmail.com>
2017-09-15 16:33:23 +0800

    1816

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def handle_message(event):
         content = crawer()
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=content.text))
+            TextSendMessage(text=content))
         return 0
     line_bot_api.reply_message(
         event.reply_token,
