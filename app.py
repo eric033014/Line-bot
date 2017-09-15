271615594e1a169e4f538606346d58220174ff35
eric <eric0330eric@gmail.com>
2017-09-15 19:41:04 +0800

    dfdf

--- app.py
+++ app.py
@@ -40,6 +40,35 @@ def callback():
 
     return 'OK'
 
+def pattern_mega(text):
+    patterns = [
+        'mega', 'mg', 'mu', 'ＭＥＧＡ', 'ＭＥ', 'ＭＵ',
+        'ｍｅ', 'ｍｕ', 'ｍｅｇａ', 'GD', 'MG', 'google',
+    ]
+    for pattern in patterns:
+        if re.search(pattern, text, re.IGNORECASE):
+            return True
+
+
+def eyny_movie():
+    target_url = 'http://www.eyny.com/forum-205-1.html'
+    print('Start parsing eynyMovie....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ''
+    for titleURL in soup.select('.bm_c tbody .xst'):
+        if pattern_mega(titleURL.text):
+            title = titleURL.text
+            if '11379780-1-3' in titleURL['href']:
+                continue
+            link = 'http://www.eyny.com/' + titleURL['href']
+            data = '{}\n{}\n\n'.format(title, link)
+            content += data
+    return content
+
+
+
 def crawer():
     target_url = 'http://www.eyny.com/forum-205-1.html'
     rs=requests.get(target_url)
@@ -83,7 +112,7 @@ def crawer():
 def handle_messag(event):
     if event.message.text == "eric":
         content = "XXXXXXXXXXXXXXXXXXXXXXXXX"
-        content = crawer()
+        content = eyny_movie()
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content))
