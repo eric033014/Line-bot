58aa12332507d3990f1dffc610677147c96e514e
Eric <eric0330eric@gmail.com>
2017-09-15 10:42:45 +0800

    tab

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import request
+import requests
 import res
 from bs4 import BeautifulSoup
 from flask import Flask, request, abort
@@ -35,20 +35,39 @@ def callback():
         abort(400)
 
     return 'OK'
-def movie():
-    target_url = 'http://www.eyny.com/forum-205-1.html'
-    rs=requests.session()
-    res=rs.get(target_url,verify=False)
-    soup=BeautifulSoup(res.text,'html.parser')
-    content=''
-    for title in soup.select('.bm_c tbody .xst'):
-	if pattern_mega(title.text):
-	    titletext=title.text
-`	    link='http://www.eyny/com/'+title['href']
-	    data='{}\n{}\n\n'.format(titletext,link)
-	    content += data
-    return content
+#def movie():
+#    target_url = 'http://www.eyny.com/forum-205-1.html'
+ #   rs=requests.session()
+  #  res=rs.get(target_url,verify=False)
+   # soup=BeautifulSoup(res.text,'html.parser')
+ #   content=''
+ #   for title in soup.select('.bm_c tbody .xst'):
+#	if pattern_mega(title.text):
+#	    titletext=title.text
+#	    link='http://www.eyny/com/'+title['href']
+#	    data='{}\n{}\n\n'.format(titletext,link)
+#	    content += data
+ #   return content
 
+
+
+
+def eyny_movie():
+    target_url = 'http://www.eyny.com/forum-205-1.html'
+    print('Start parsing eynyMovie....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ''
+    for titleURL in soup.select('.bm_c tbody .xst'):
+        if pattern_mega(titleURL.text):
+            title = titleURL.text
+            if '11379780-1-3' in titleURL['href']:
+                continue
+            link = 'http://www.eyny.com/' + titleURL['href']
+            data = '{}\n{}\n\n'.format(title, link)
+            content += data
+    return content
 
 
 
@@ -58,7 +77,7 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
     if enent.message.text == 'eyny':
-	content=movie()
+	content=eyny_movie()
 	line_bot_api.reply_message(
 	    event.reply_token,
             TextSendMessage(text=content))
