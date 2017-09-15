690da0a2883633d8443628035faf90d7d8b1bafe
Eric <eric0330eric@gmail.com>
2017-09-15 10:10:05 +0800

    ok

--- app.py
+++ app.py
@@ -39,7 +39,29 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+    if enent.message.text == 'eyny':
+	content=movie()
+	line_bot_api.reply_message(
+	    event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+
+
+def movie():
+    target_url = 'http://www.eyny.com/forum-205-1.html'
+    rs=requests.session()
+    res=rs.get(target_url,verify=False)
+    soup=BeautifulSoup(res.text,'html.parser')
+    content=''
+    for title in soup.select('.bm_c tbody .xst'):
+	if pattern_mega(title.text)
+	    titletext=title.text
+`	    link='http://www.eyny/com/'+title['href']
+	    data='{}\n{}\n\n'.format(title,link)
+	    content += data
+    return content
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(i)
