cadcc7c748371bb559611c59fdc4729641f58000
Eric <eric0330eric@gmail.com>
2017-09-15 11:30:52 +0800

    ok

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def eyny_movie():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == "eric":
-        TextSendMessage("Hello Eric")
+        TextSendMessage(text="Hello Eric")
         content = eyny_movie()
         line_bot_api.reply_message(
             event.reply_token,
