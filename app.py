cea26efa632608c23e07b19eb176e8af9f253d52
Eric <eric0330eric@gmail.com>
2017-09-15 10:48:33 +0800

    tab

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-    if enent.message.text == 'eyny':
+    if event.message.text == "eyny" :
 	content=eyny_movie()
 	line_bot_api.reply_message(
 	    event.reply_token,
