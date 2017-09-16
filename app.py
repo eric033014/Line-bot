26b596e4c69c7a2cbcc5b1ce113170373c47f162
eric <eric0330eric@gmail.com>
2017-09-16 23:15:31 +0800

    cs

--- app.py
+++ app.py
@@ -79,32 +79,14 @@ def beauty():
     for i in soup.select('.r-ent .title'):
         title=i.text.encode('utf-8')
         #print(i.text)
-        if i.find('a'):
-            link = 'https://www.ptt.cc'+i.find('a')['href']
-            data = '{}\n{}\n\n'.format(title,link)
-            content+=data
-    return content
-
-
-
-"""def eyny_movie():
-    target_url = 'http://www.eyny.com/forum-205-1.html'
-    print('Start parsing eynyMovie....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    res.encoding= 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for titleURL in soup.select('.bm_c tbody .xst'):
         if pattern_mega(titleURL.text):
             title = titleURL.text
-            if '11379780-1-3' in titleURL['href']:
-                continue
-            link = 'http://www.eyny.com/' + titleURL['href']
-            data = '{}\n{}\n\n'.format(title, link)
-            content += data
+            if i.find('a'):
+                link = 'https://www.ptt.cc'+i.find('a')['href']
+                data = '{}\n{}\n\n'.format(title,link)
+                content+=data
     return content
-"""
+
 
 
 @handler.add(MessageEvent, message=TextMessage)
