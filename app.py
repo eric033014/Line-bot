6512f73a549d35e1d1aeaba0e151745bcdfa7488
eric <eric0330eric@gmail.com>
2017-09-17 01:24:31 +0800

    d

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def beauty():
         #title=i.text.encode('utf-8')
         #print(i.text)
         if i.find('a'):
-            title=r_ent.find(class_="title").text.strip()
+            title=.find(class_="title").text.strip()
             link = 'https://www.ptt.cc'+i.find('a')['href']
 
             data = '{}\n{}\n\n'.format(title,link)
